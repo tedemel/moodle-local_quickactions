e30caea8ba9ba8d27fa96f089d421cc30864a0e6
Tessa Demel <254993102+tedemel@users.noreply.github.com>
2026-05-19 21:16:47 +0200

    chore: promote to STABLE and bump release to 1.0.0
    - Promote $plugin->maturity from MATURITY_ALPHA to MATURITY_STABLE
    - Bump version 2026050500 -> 2026051903
    - Bump release 0.2.0 -> 1.0.0

--- version.php
+++ version.php
@@ -25,9 +25,9 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_quickactions';
-$plugin->version   = 2026050500;
-$plugin->release   = '0.2.0';
-$plugin->requires  = 2025041400;
+$plugin->version   = 2026051903;
+$plugin->release   = '1.0.0';
+$plugin->requires  = 2025041400; // Moodle 5.0.
 $plugin->supported = [500, 502];
-$plugin->maturity  = MATURITY_ALPHA;
+$plugin->maturity  = MATURITY_STABLE;
 $plugin->dependencies = [];
